5f116a0de1427a746d469ea1f7ece9c397a36477
Ashton A <uhdbits@duck.com>
2024-12-31 16:54:00 -0700

    Add CommandSwerveDrivetrain and DriveIO
    CommandSwerveDrivetrain acts as a "constants" holder for the TunerConstants, and DriveIO will be used to log values from SwerveDrivetrain and log them in an semi-AdvantageKit compatible way.
    
    Signed-off-by: Ashton A <uhdbits@duck.com>

--- src/main/java/com/team1165/robot/subsystems/drive/DriveIO.java
+++ src/main/java/com/team1165/robot/subsystems/drive/DriveIO.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2024 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+ * File originally made by: The Cheesy Poofs - FRC 254
+ * Copyright (c) 2024 Team 254 (https://github.com/Team254)
+ * Copyright (c) 2025 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
  *
  * Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
  * the root directory of this project.
@@ -7,4 +9,34 @@
 
 package com.team1165.robot.subsystems.drive;
 
-public class DriveIO {}
+import com.ctre.phoenix6.swerve.SwerveDrivetrain;
+import edu.wpi.first.math.geometry.Pose2d;
+import org.littletonrobotics.junction.AutoLog;
+
+/**
+ * A "hardware" interface/implementation layer for a Swerve Drivetrain powered by the CTRE Swerve
+ * Library. Due to the nature of the CTRE Swerve Library, this IO class is limited in replay
+ * capabilities.
+ */
+public class DriveIO {
+  /** Class used to store the IO values of a CTRE Swerve Drivetrain. */
+  @AutoLog
+  class DriveIOInputs extends SwerveDrivetrain.SwerveDriveState {
+    DriveIOInputs() {
+      this.Pose = new Pose2d();
+    }
+
+    public void fromSwerveDriveState(SwerveDrivetrain.SwerveDriveState state) {
+      this.Pose = state.Pose;
+      this.Speeds = state.Speeds;
+      this.ModuleStates = state.ModuleStates;
+      this.ModuleTargets = state.ModuleTargets;
+      this.ModulePositions = state.ModulePositions;
+      this.RawHeading = state.RawHeading;
+      this.Timestamp = state.Timestamp;
+      this.OdometryPeriod = state.OdometryPeriod;
+      this.SuccessfulDaqs = state.SuccessfulDaqs;
+      this.FailedDaqs = state.FailedDaqs;
+    }
+  }
+}
---
+++ src/main/java/com/team1165/robot/subsystems/drive/constants/CommandSwerveDrivetrain.java
@@ -0,0 +1,45 @@
+/*
+ * File originally made by: The Cheesy Poofs - FRC 254
+ * Copyright (c) 2024 Team 254 (https://github.com/Team254)
+ * Copyright (c) 2025 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+ *
+ * Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
+ * the root directory of this project.
+ */
+
+package com.team1165.robot.subsystems.drive.constants;
+
+import com.ctre.phoenix6.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+
+/**
+ * A simple class that allows us to hold the constants from the TunerConstants output by the Phoenix
+ * Tuner X Swerve Generator. This allows us to use the TunerConstants without having to modify the
+ * file heavily.
+ */
+public class CommandSwerveDrivetrain {
+  SwerveDrivetrainConstants drivetrainConstants;
+  SwerveModuleConstants[] moduleConstants;
+
+  /**
+   * A simple class that allows us to hold the constants from the TunerConstants output by the
+   * Phoenix Tuner X Swerve Generator. This allows us to use the TunerConstants without having to
+   * modify the file heavily.
+   *
+   * @param drivetrainConstants Drivetrain-wide constants for the swerve drive.
+   * @param modules Constants for each specific module.
+   */
+  public CommandSwerveDrivetrain(
+      SwerveDrivetrainConstants drivetrainConstants, SwerveModuleConstants... modules) {
+    this.drivetrainConstants = drivetrainConstants;
+    this.moduleConstants = modules;
+  }
+
+  public SwerveDrivetrainConstants getDrivetrainConstants() {
+    return drivetrainConstants;
+  }
+
+  public SwerveModuleConstants[] getModuleConstants() {
+    return moduleConstants;
+  }
+}
