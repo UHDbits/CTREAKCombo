b2752c3cf8a4c5616395abbd31090fa43751bbc6
Ashton A <uhdbits@duck.com>
2024-12-31 12:29:08 -0700

    Add IDEA configuration files
    Add various IDEA configuration files for the project. The current code style doesn't exactly match up with what spotless produces, so I may try to mess around with that a bit.
    
    Signed-off-by: Ashton A <uhdbits@duck.com>

--- src/main/java/com/team1165/robot/Main.java
+++ src/main/java/com/team1165/robot/Main.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+// Copyright (c) 2025 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
 //
 // Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
 // the root directory of this project.
