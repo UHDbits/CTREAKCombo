c465fb205082a5885b69eda157ec860c8561b19c
Ashton A <uhdbits@duck.com>
2024-12-31 11:45:55 -0700

    Add basic project files
    Add basic WPILib project files in order to expand upon.
    
    Signed-off-by: Ashton A <uhdbits@duck.com>

---
+++ src/main/java/com/team1165/robot/Main.java
@@ -0,0 +1,26 @@
+// Copyright (c) 2024 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+//
+// Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
+// the root directory of this project.
+
+package com.team1165.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+/**
+ * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
+ * you are doing, do not modify this file except to change the parameter class to the startRobot
+ * call.
+ */
+public final class Main {
+  private Main() {}
+
+  /**
+   * Main initialization method. Do not perform any initialization here.
+   *
+   * <p>If you change your main Robot class (name), change the parameter type.
+   */
+  public static void main(String... args) {
+    RobotBase.startRobot(Robot::new);
+  }
+}
---
+++ src/main/java/com/team1165/robot/Robot.java
@@ -0,0 +1,72 @@
+// Copyright (c) 2024 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+//
+// Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
+// the root directory of this project.
+
+package com.team1165.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import org.littletonrobotics.junction.LoggedRobot;
+
+public class Robot extends LoggedRobot {
+  private Command autonomousCommand;
+
+  private RobotContainer robotContainer;
+
+  @Override
+  public void robotInit() {}
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  @Override
+  public void autonomousInit() {
+    autonomousCommand = robotContainer.getAutonomousCommand();
+
+    if (autonomousCommand != null) {
+      autonomousCommand.schedule();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
+
+  @Override
+  public void teleopInit() {
+    if (autonomousCommand != null) {
+      autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void teleopExit() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void testExit() {}
+}
