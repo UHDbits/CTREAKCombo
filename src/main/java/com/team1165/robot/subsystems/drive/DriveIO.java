d9744a77171889ec981a1d72ad187256b463b1f8
Ashton A <uhdbits@duck.com>
2025-01-01 09:39:22 -0700

    Finish up DriveIOReal
    Finish up DriveIOReal with logging capabiltiies and it's constructors, going to add DriveIOSim next.
    
    Signed-off-by: Ashton A <uhdbits@duck.com>

--- src/main/java/com/team1165/robot/subsystems/drive/DriveIO.java
+++ src/main/java/com/team1165/robot/subsystems/drive/DriveIO.java
@@ -29,7 +29,7 @@ import org.littletonrobotics.junction.AutoLog;
  * AdvantageKit compatible way. Any new features or customization that can be done outside of this
  * communication layer should be done in the actual Drive subsystem.
  */
-public class DriveIO {
+public interface DriveIO {
   /** Class used to store the IO values of a CTRE Swerve Drivetrain. */
   @AutoLog
   class DriveIOInputs extends SwerveDrivetrain.SwerveDriveState {
@@ -53,181 +53,163 @@ public class DriveIO {
     }
   }
 
-
   /**
    * Updates a {@link DriveIOInputs} instance with the latest updates from this {@link DriveIO}.
    *
    * @param inputs A {@link DriveIOInputs} instance to update.
    */
-  void updateInputs(DriveIOInputs inputs) {}
+  default void updateInputs(DriveIOInputs inputs) {}
 
   /**
    * Logs specific additional information about the swerve modules, like temperature, applied
    * output, current, etc.
    */
-  void logModules(SwerveDrivetrain.SwerveDriveState driveState) {}
+  default void logModules() {}
 
   /**
    * Applies the specified control request to this swerve drivetrain.
    *
    * @param request Request to apply
    */
-  void setControl(SwerveRequest request) {}
+  default void setControl(SwerveRequest request) {}
 
   /**
-   * Register the specified lambda to be executed whenever our SwerveDriveState function
-   * is updated in our odometry thread.
-   * <p>
-   * It is imperative that this function is cheap, as it will be executed along with
-   * the odometry call, and if this takes a long time, it may negatively impact the
-   * odometry of this stack.
-   * <p>
-   * This can also be used for logging data if the function performs logging instead of telemetry.
-   * Additionally, the SwerveDriveState object can be cloned and stored for later processing.
+   * Register the specified lambda to be executed whenever our SwerveDriveState function is updated
+   * in our odometry thread.
+   *
+   * <p>It is imperative that this function is cheap, as it will be executed along with the odometry
+   * call, and if this takes a long time, it may negatively impact the odometry of this stack.
+   *
+   * <p>This can also be used for logging data if the function performs logging instead of
+   * telemetry. Additionally, the SwerveDriveState object can be cloned and stored for later
+   * processing.
    *
    * @param telemetryFunction Function to call for telemetry or logging
    */
-  void registerTelemetry(Consumer<SwerveDriveState> telemetryFunction) {}
+  default void registerTelemetry(Consumer<SwerveDriveState> telemetryFunction) {}
 
   /**
-   * Configures the neutral mode to use for all modules' drive motors.
+   * Changes the neutral mode to use for all modules' drive motors.
    *
-   * @param neutralMode The drive motor neutral mode
+   * @param neutralMode The new drive motor neutral mode
    */
-  void configNeutralMode(NeutralModeValue neutralMode) {}
+  default void changeNeutralMode(NeutralModeValue neutralMode) {}
 
   /**
    * Zero's this swerve drive's odometry entirely.
-   * <p>
-   * This will zero the entire odometry, and place the robot at 0,0
+   *
+   * <p>This will zero the entire odometry, and place the robot at 0,0
    */
-  void tareEverything() {}
+  default void tareEverything() {}
 
   /**
-   * Resets the rotation of the robot pose to 0 from the
-   * {@link SwerveRequest.ForwardPerspectiveValue#OperatorPerspective}
-   * perspective. This makes the current orientation of the robot X
-   * forward for field-centric maneuvers.
-   * <p>
-   * This is equivalent to calling {@link #resetRotation} with the operator
-   * perspective rotation.
+   * Resets the rotation of the robot pose to 0 from the {@link
+   * SwerveRequest.ForwardPerspectiveValue#OperatorPerspective} perspective. This makes the current
+   * orientation of the robot X forward for field-centric maneuvers.
+   *
+   * <p>This is equivalent to calling {@link #resetRotation} with the operator perspective rotation.
    */
-  void seedFeedCentric() {}
+  default void seedFeedCentric() {}
 
   /**
-   * Resets the pose of the robot. The pose should be from the
-   * {@link SwerveRequest.ForwardPerspectiveValue#BlueAlliance} perspective.
+   * Resets the pose of the robot. The pose should be from the {@link
+   * SwerveRequest.ForwardPerspectiveValue#BlueAlliance} perspective.
    *
    * @param pose Pose to make the current pose
    */
-  void resetPose(Pose2d pose) {}
+  default void resetPose(Pose2d pose) {}
 
   /**
-   * Resets the translation of the robot pose without affecting rotation.
-   * The translation should be from the {@link SwerveRequest.ForwardPerspectiveValue#BlueAlliance}
-   * perspective.
+   * Resets the translation of the robot pose without affecting rotation. The translation should be
+   * from the {@link SwerveRequest.ForwardPerspectiveValue#BlueAlliance} perspective.
    *
    * @param translation Translation to make the current translation
    */
-  void resetTranslation(Translation2d translation) {}
+  default void resetTranslation(Translation2d translation) {}
 
   /**
-   * Resets the rotation of the robot pose without affecting translation.
-   * The rotation should be from the {@link SwerveRequest.ForwardPerspectiveValue#BlueAlliance}
-   * perspective.
+   * Resets the rotation of the robot pose without affecting translation. The rotation should be
+   * from the {@link SwerveRequest.ForwardPerspectiveValue#BlueAlliance} perspective.
    *
    * @param rotation Rotation to make the current rotation
    */
-  void resetRotation(Rotation2d rotation) {}
+  default void resetRotation(Rotation2d rotation) {}
 
   /**
-   * Takes the {@link SwerveRequest.ForwardPerspectiveValue#BlueAlliance} perpective direction
-   * and treats it as the forward direction for
-   * {@link SwerveRequest.ForwardPerspectiveValue#OperatorPerspective}.
-   * <p>
-   * If the operator is in the Blue Alliance Station, this should be 0 degrees.
-   * If the operator is in the Red Alliance Station, this should be 180 degrees.
-   * <p>
-   * This does not change the robot pose, which is in the
-   * {@link SwerveRequest.ForwardPerspectiveValue#BlueAlliance} perspective.
-   * As a result, the robot pose may need to be reset using {@link #resetPose}.
+   * Takes the {@link SwerveRequest.ForwardPerspectiveValue#BlueAlliance} perpective direction and
+   * treats it as the forward direction for {@link
+   * SwerveRequest.ForwardPerspectiveValue#OperatorPerspective}.
    *
-   * @param fieldDirection Heading indicating which direction is forward from
-   *                       the {@link SwerveRequest.ForwardPerspectiveValue#BlueAlliance} perspective
-   */
-  void setOperatorPerspectiveForward(Rotation2d fieldDirection) {}
-
-  /**
-   * Adds a vision measurement to the Kalman Filter. This will correct the
-   * odometry pose estimate while still accounting for measurement noise.
-   * <p>
-   * This method can be called as infrequently as you want, as long as you are
-   * calling {@link SwerveDrivePoseEstimator#update} every loop.
-   * <p>
-   * To promote stability of the pose estimate and make it robust to bad vision
-   * data, we recommend only adding vision measurements that are already within
-   * one meter or so of the current pose estimate.
+   * <p>If the operator is in the Blue Alliance Station, this should be 0 degrees. If the operator
+   * is in the Red Alliance Station, this should be 180 degrees.
    *
-   * @param visionRobotPoseMeters The pose of the robot as measured by the vision
-   *                              camera.
-   * @param timestampSeconds      The timestamp of the vision measurement in
-   *                              seconds. Note that you must use a timestamp with an
-   *                              epoch since system startup (i.e., the epoch of this
-   *                              timestamp is the same epoch as
-   *                              {@link Utils#getCurrentTimeSeconds}).
-   *                              This means that you should use
-   *                              {@link Utils#getCurrentTimeSeconds}
-   *                              as your time source or sync the epochs.
-   *                              An FPGA timestamp can be converted to the correct
-   *                              timebase using {@link Utils#fpgaToCurrentTime}.
+   * <p>This does not change the robot pose, which is in the {@link
+   * SwerveRequest.ForwardPerspectiveValue#BlueAlliance} perspective. As a result, the robot pose
+   * may need to be reset using {@link #resetPose}.
+   *
+   * @param fieldDirection Heading indicating which direction is forward from the {@link
+   *     SwerveRequest.ForwardPerspectiveValue#BlueAlliance} perspective
    */
-  void addVisionMeasurement(Pose2d visionRobotPoseMeters, double timestampSeconds) {}
+  default void setOperatorPerspectiveForward(Rotation2d fieldDirection) {}
 
   /**
-   * Adds a vision measurement to the Kalman Filter. This will correct the
-   * odometry pose estimate while still accounting for measurement noise.
-   * <p>
-   * This method can be called as infrequently as you want, as long as you are
-   * calling {@link SwerveDrivePoseEstimator#update} every loop.
-   * <p>
-   * To promote stability of the pose estimate and make it robust to bad vision
-   * data, we recommend only adding vision measurements that are already within
-   * one meter or so of the current pose estimate.
-   * <p>
-   * Note that the vision measurement standard deviations passed into this method
-   * will continue to apply to future measurements until a subsequent call to {@link
+   * Adds a vision measurement to the Kalman Filter. This will correct the odometry pose estimate
+   * while still accounting for measurement noise.
+   *
+   * <p>This method can be called as infrequently as you want, as long as you are calling {@link
+   * SwerveDrivePoseEstimator#update} every loop.
+   *
+   * <p>To promote stability of the pose estimate and make it robust to bad vision data, we
+   * recommend only adding vision measurements that are already within one meter or so of the
+   * current pose estimate.
+   *
+   * @param visionRobotPoseMeters The pose of the robot as measured by the vision camera.
+   * @param timestampSeconds The timestamp of the vision measurement in seconds. Note that you must
+   *     use a timestamp with an epoch since system startup (i.e., the epoch of this timestamp is
+   *     the same epoch as {@link Utils#getCurrentTimeSeconds}). This means that you should use
+   *     {@link Utils#getCurrentTimeSeconds} as your time source or sync the epochs. An FPGA
+   *     timestamp can be converted to the correct timebase using {@link Utils#fpgaToCurrentTime}.
+   */
+  default void addVisionMeasurement(Pose2d visionRobotPoseMeters, double timestampSeconds) {}
+
+  /**
+   * Adds a vision measurement to the Kalman Filter. This will correct the odometry pose estimate
+   * while still accounting for measurement noise.
+   *
+   * <p>This method can be called as infrequently as you want, as long as you are calling {@link
+   * SwerveDrivePoseEstimator#update} every loop.
+   *
+   * <p>To promote stability of the pose estimate and make it robust to bad vision data, we
+   * recommend only adding vision measurements that are already within one meter or so of the
+   * current pose estimate.
+   *
+   * <p>Note that the vision measurement standard deviations passed into this method will continue
+   * to apply to future measurements until a subsequent call to {@link
    * SwerveDrivePoseEstimator#setVisionMeasurementStdDevs(Matrix)} or this method.
    *
-   * @param visionRobotPoseMeters    The pose of the robot as measured by the
-   *                                 vision camera.
-   * @param timestampSeconds         The timestamp of the vision measurement in
-   *                                 seconds. Note that you must use a timestamp with an
-   *                                 epoch since system startup (i.e., the epoch of this
-   *                                 timestamp is the same epoch as
-   *                                 {@link Utils#getCurrentTimeSeconds}).
-   *                                 This means that you should use
-   *                                 {@link Utils#getCurrentTimeSeconds}
-   *                                 as your time source or sync the epochs.
-   *                                 An FPGA timestamp can be converted to the correct
-   *                                 timebase using {@link Utils#fpgaToCurrentTime}.
-   * @param visionMeasurementStdDevs Standard deviations of the vision pose
-   *                                 measurement (x position
-   *                                 in meters, y position in meters, and heading
-   *                                 in radians). Increase these numbers to trust
-   *                                 the vision pose measurement less.
+   * @param visionRobotPoseMeters The pose of the robot as measured by the vision camera.
+   * @param timestampSeconds The timestamp of the vision measurement in seconds. Note that you must
+   *     use a timestamp with an epoch since system startup (i.e., the epoch of this timestamp is
+   *     the same epoch as {@link Utils#getCurrentTimeSeconds}). This means that you should use
+   *     {@link Utils#getCurrentTimeSeconds} as your time source or sync the epochs. An FPGA
+   *     timestamp can be converted to the correct timebase using {@link Utils#fpgaToCurrentTime}.
+   * @param visionMeasurementStdDevs Standard deviations of the vision pose measurement (x position
+   *     in meters, y position in meters, and heading in radians). Increase these numbers to trust
+   *     the vision pose measurement less.
    */
-  void addVisionMeasurement(Pose2d visionRobotPoseMeters, double timestampSeconds,  Matrix<N3, N1> visionMeasurementStdDevs) {}
+  default void addVisionMeasurement(
+      Pose2d visionRobotPoseMeters,
+      double timestampSeconds,
+      Matrix<N3, N1> visionMeasurementStdDevs) {}
 
   /**
-   * Sets the pose estimator's trust of global measurements. This might be used to
-   * change trust in vision measurements after the autonomous period, or to change
-   * trust as distance to a vision target increases.
+   * Sets the pose estimator's trust of global measurements. This might be used to change trust in
+   * vision measurements after the autonomous period, or to change trust as distance to a vision
+   * target increases.
    *
-   * @param visionMeasurementStdDevs Standard deviations of the vision
-   *                                 measurements. Increase these numbers to
-   *                                 trust global measurements from vision less.
-   *                                 This matrix is in the form [x, y, theta]ᵀ,
-   *                                 with units in meters and radians.
+   * @param visionMeasurementStdDevs Standard deviations of the vision measurements. Increase these
+   *     numbers to trust global measurements from vision less. This matrix is in the form [x, y,
+   *     theta]ᵀ, with units in meters and radians.
    */
-  void setVisionMeasurementStdDevs(Matrix<N3, N1> visionMeasurementStdDevs) {}
+  default void setVisionMeasurementStdDevs(Matrix<N3, N1> visionMeasurementStdDevs) {}
 }
